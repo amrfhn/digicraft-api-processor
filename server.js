020abf50bd9748ee53c1351b2ce2983f3986c1d2
Amir Farhan bin Jefri <amirfarhan.jfri@gmail.com>
2022-06-29 02:54:01 +0800

    port change

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const app = express();
 const db = mongoose.connection;
 
+const port = process.env.PORT || 3030;
 const password = 'Farhan94'
 
 mongoose.connect(
@@ -22,7 +23,7 @@ app.use(express.json())
 db.on('error', (error) => console.error(error));
 db.once('open', () => console.log('Connected to Database'));
 
-app.listen(3030, () => console.log('Server started'));
+app.listen(port, () => console.log('Server started'));
 
 const rsvpRouter = require('./routes/rsvp')
 app.use('/rsvp', rsvpRouter);
