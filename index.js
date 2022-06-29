165c15e49603aa54b27a0aa0b5eca968620c6ebd
Amir Farhan bin Jefri <amirfarhan.jfri@gmail.com>
2022-06-30 00:56:17 +0800

    change HTTP

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const http = require('http');
 require('dotenv').config()
 
 const app = express();
 app.use(express.static('public'));
 app.use(express.json())
 
+const httpServer = http.createServer(app);
+
 const db = mongoose.connection;
 
 const port = process.env.PORT || 3030;
+const http_port = process.env.HTTP_PORT || 8000;
 const password = 'Farhan94';
 
 mongoose.connect(
@@ -27,10 +31,7 @@ db.on('error', (error) => console.error(error));
 db.once('open', () => console.log('Connected to Database'));
 
 app.listen(port, () => console.log('Server started'));
-app.on('clientError', (err, socket) => {
-  console.error(err);
-  socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
-});
+httpServer.listen(http_port);
 
 const rsvpRouter = require('./routes/rsvp')
 app.use('/rsvp', rsvpRouter);
