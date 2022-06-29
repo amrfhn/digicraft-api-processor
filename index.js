e03cc3b21e98fa9d8dc1648d17bc55d056b0cfae
Amir Farhan bin Jefri <amirfarhan.jfri@gmail.com>
2022-06-30 01:20:47 +0800

    change HTtp

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const http = require('http');
+const sslRedirect = require('heroku-ssl-redirect').default;
 require('dotenv').config()
 
 const app = express();
+app.use(sslRedirect());
 app.use(express.static('public'));
 app.use(express.json())
-
-const httpServer = http.createServer(app);
 
 const db = mongoose.connection;
 
 const port = process.env.PORT || 3030;
-const http_port = process.env.HTTP_PORT || 8000;
 const password = 'Farhan94';
 
 mongoose.connect(
@@ -31,7 +29,6 @@ db.on('error', (error) => console.error(error));
 db.once('open', () => console.log('Connected to Database'));
 
 app.listen(port, () => console.log('Server started'));
-httpServer.listen(http_port);
 
 const rsvpRouter = require('./routes/rsvp')
 app.use('/rsvp', rsvpRouter);
