fd455204c37cc37ea5f3f7edf84abe284c39b937
Amir Farhan bin Jefri <amirfarhan.jfri@gmail.com>
2022-06-30 00:22:08 +0800

    fixed error

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
+require('dotenv').config()
 
 const app = express();
+app.use(express.json())
+
 const db = mongoose.connection;
 
 const port = process.env.PORT || 3030;
-const password = 'Farhan94'
+const password = process.env.DB_PASSWORD;
 
 mongoose.connect(
   `mongodb+srv://digicraftAdmin:${password}@digicraft-central.rjug2zb.mongodb.net/rsvpListing?retryWrites=true&w=majority`,
@@ -17,7 +20,6 @@ mongoose.connect(
   }
 );
 
-app.use(express.json())
 
 
 db.on('error', (error) => console.error(error));
--- public/main.js
+++ public/main.js
@@ -1,14 +1,17 @@
 function run() {
-  fetch("/api/movie")
-    .then((response) => response.json())
-    .then((data) => {
-        console.log(data);
-    //   const detailsElement = document.getElementById("movie");
+//   fetch("/rsvp")
+//     .then((response) => response.json())
+//     .then((data) => {
+//       console.log(data);
+      //   const detailsElement = document.getElementById("movie");
 
-    //   detailsElement.getElementsByTagName("img")[0].src = data.poster;
-    //   detailsElement.getElementsByTagName("h1")[0].innerText = data.title;
-    //   detailsElement.getElementsByTagName("p")[0].innerText = data.fullplot;
+      //   detailsElement.getElementsByTagName("img")[0].src = data.poster;
+      //   detailsElement.getElementsByTagName("h1")[0].innerText = data.title;
+      //   detailsElement.getElementsByTagName("p")[0].innerText = data.fullplot;
 
-    //   detailsElement.style.visibility = "visible";
-    });
+      //   detailsElement.style.visibility = "visible";
+    // });
+
+
+    console.log('tsajskjaksjk')
 }
