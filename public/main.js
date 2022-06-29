61fac4375a0ccd84cbc7fd6900a3380ad0278c2d
Amir Farhan bin Jefri <amirfarhan.jfri@gmail.com>
2022-06-29 23:35:02 +0800

    clean up

---
+++ public/main.js
@@ -0,0 +1,14 @@
+function run() {
+  fetch("/api/movie")
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data);
+    //   const detailsElement = document.getElementById("movie");
+
+    //   detailsElement.getElementsByTagName("img")[0].src = data.poster;
+    //   detailsElement.getElementsByTagName("h1")[0].innerText = data.title;
+    //   detailsElement.getElementsByTagName("p")[0].innerText = data.fullplot;
+
+    //   detailsElement.style.visibility = "visible";
+    });
+}
