df43f92a586f585307c968893410723bb30f6891
Amir Farhan bin Jefri <amirfarhan.jfri@gmail.com>
2022-06-30 20:28:56 +0800

    cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require('cors')
 const mongoose = require("mongoose");
 require('dotenv').config()
 
 const app = express();
+app.use(cors())
 app.use(express.static('public'));
 app.use(express.json())
 
