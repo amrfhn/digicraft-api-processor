fe86c3e91bdb041887615d9aa921cf893a2b6be0
Amir Farhan bin Jefri <amirfarhan.jfri@gmail.com>
2022-07-04 02:08:10 +0800

    update env and add new table

--- index.js
+++ index.js
@@ -11,10 +11,13 @@ app.use(express.json())
 const db = mongoose.connection;
 
 const port = process.env.PORT || 3030;
-const password = 'Farhan94';
+const password = process.env.DB_PASSWORD;
+const username = process.env.DB_USERNAME;
+const db_name = process.env.DB_NAME;
+
 
 mongoose.connect(
-  `mongodb+srv://digicraftAdmin:${password}@digicraft-central.rjug2zb.mongodb.net/rsvpListing?retryWrites=true&w=majority`,
+  `mongodb+srv://${username}:${password}@digicraft-central.rjug2zb.mongodb.net/${db_name}?retryWrites=true&w=majority`,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -30,5 +33,5 @@ db.once('open', () => console.log('Connected to Database'));
 
 app.listen(port, () => console.log('Server started'));
 
-const rsvpRouter = require('./routes/rsvp')
+const rsvpRouter = require('./routes/rsvp') 
 app.use('/rsvp', rsvpRouter);
