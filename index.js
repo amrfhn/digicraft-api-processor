9fa6fe67afa9f2275756f369085b2a9c0407b208
Amir Farhan bin Jefri <jefriaf@basfad.basf.net>
2023-06-27 00:43:37 +0800

    add cors whitelist

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const mongoose = require("mongoose");
 require('dotenv').config()
 
 const app = express();
-app.use(cors())
+app.use(cors({
+  origin: ['http://localhost:3000/']
+}))
+
 app.use(express.static('public'));
 app.use(express.json())
 
