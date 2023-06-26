cb52a36f63f4176fa370e403e6eaa4a3dc21da0a
Amir Farhan bin Jefri <jefriaf@basfad.basf.net>
2023-06-27 00:46:15 +0800

    add cors whitelist

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require('dotenv').config()
 
 const app = express();
 app.use(cors({
-  origin: ['http://localhost:3000/']
+  origin: ['http://localhost:3000/', 'https://digital-invite-202301.digicraft.link/']
 }))
 
 app.use(express.static('public'));
