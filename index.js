6da4e01aba2b4f19c7987f57c40123cc248afe6d
Amir Farhan bin Jefri <jefriaf@basfad.basf.net>
2023-06-28 22:33:27 +0800

    ready

--- index.js
+++ index.js
@@ -19,6 +19,8 @@ const password = process.env.DB_PASSWORD;
 const username = process.env.DB_USERNAME;
 const db_name = process.env.DB_NAME;
 
+mongoose.set("strictQuery", false);
+
 mongoose.connect(
   `mongodb+srv://${username}:${password}@digicraft-central.rjug2zb.mongodb.net/${db_name}?retryWrites=true&w=majority`,
   {
